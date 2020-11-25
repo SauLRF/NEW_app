9a13f555bdc16588b88d846151ca2ccddd00ccf9
Ruslan <ruslan.sattarov.f@gmail.com>
2020-11-25 20:25:17 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import ItemCard from "./Contact.js";
+
+class App extends React.Component{
+  state = {
+      todos:[],
+  }
+  componentDidMount(){
+     fetch("http://localhost:3000/todos.json")
+     .then(r=>r.json())
+     .then(data=>this.setState({todos:data}))
+     
+  }
+  render(){
+  return(
+    <React.Fragment>
+      dsdsd
+    </React.Fragment>
+  )
+  }
+  
+}
+
+
+export default App;
