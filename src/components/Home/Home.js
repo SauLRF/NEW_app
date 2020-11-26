44b0725794c083ca7a3fbb41fb6d06b56bce2136
Ruslan <ruslan.sattarov.f@gmail.com>
2020-11-26 16:45:27 +0300

    commit_26

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,14 +3,14 @@ import './Home.scss'
 
 import Header from '../Header/Header.js'
 
-import { ReactComponent as User } from '../../images/user.svg'
+
 import { ReactComponent as Star } from '../../images/star.svg'
-import { ReactComponent as Nurse } from '../../images/nurse.svg'
+
 import { ReactComponent as House } from '../../images/house.svg'
-import { ReactComponent as Clients } from '../../images/clients.svg'
-import { ReactComponent as Messages } from '../../images/messages.svg'
+
+
 import { ReactComponent as Broadcast } from '../../images/broadcast.svg'
-import { ReactComponent as Employees } from '../../images/employees.svg'
+
 import { ReactComponent as Appointment } from '../../images/appointment.svg'
 
 const TITLE="Домашняя";
@@ -18,9 +18,6 @@ const SECTIONS = [
     { title: 'Приёмы', href: '/appointments', Icon: Appointment },
     { title: 'События', href: '/events', Icon: Star  },
     { title: 'Оповещения', href: '/notifications', Icon: Broadcast },
-    { title: 'Сообщения', href: '/messages', Icon: Messages },
-    { title: 'Клиенты', href: '/clients', Icon: Clients },
-    { title: 'Сотрудники', href: '/employees', Icon: Employees }
   ];
 
   export default class Home extends Component {
@@ -32,8 +29,11 @@ const SECTIONS = [
                        userName="Ruslan Sattarov Faritovich"
                        className="Home-Header"
                        renderIcon={()=>(<House className="Header-Icon"/>)} 
-                     />        
+                     />  
                    <div className="Home-Body">
+                         WELCOME TO APP
+                        </div>      
+                   <div className="Home-Footer">
                      <div className="SectionNavigation">
                         {SECTIONS.map(({title,Icon})=>{
                             return( <a className='SectionNavigation-Item Section' href='#'>
