f5e98d0faac87295ebb593796281d538d1f9fbe7
Ruslan <ruslan.sattarov.f@gmail.com>
2020-11-26 08:35:49 +0300

    new app

---
+++ src/NEW_app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Home from "./components/Home/Home.js";
+
+
+class App extends React.Component{
+
+  render(){
+  return(
+    <div className="App">
+          <Home/>
+          sdfsdfd
+    </div>
+    )
+} }
+
+
+export default App;
